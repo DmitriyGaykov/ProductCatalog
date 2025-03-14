f7cd1d491f42f76e7daf849a4ad88aa51fb9714f
Dmitriy <dima.gaykov.04@gmail.com>
2025-03-15 02:27:44 +0300

    UsersPage на клиенте

---
+++ ProductCatalog.Client/src/hooks/useRemoveUser.ts
@@ -0,0 +1,28 @@
+import {useRemoveUserMutation} from "../store";
+import {useCallback} from "react";
+import {toast} from "react-toastify";
+import {extractErrors} from "../utils";
+
+export const useUserRemove = () => {
+  const [removeUser, { isLoading }] = useRemoveUserMutation();
+
+  const _removeUser = useCallback(async (userId: string) => {
+    try {
+      const {data, error} = await removeUser({ userId })
+      if (error) {
+        const errors = extractErrors(error);
+        errors.forEach(e => toast.error(e));
+        return;
+      }
+
+      return data;
+    } catch (e) {
+      toast.error("Ошибка при удалении пользователя")
+    }
+  },[removeUser]);
+
+  return {
+    removeUser: _removeUser,
+    isLoading,
+  }
+}
